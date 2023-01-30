2710c80f18ddd5b206448c86becfbf5dc7b7e278
Julia <ajulia.fernanda80@gmail.com>
2023-01-30 00:29:20 -0300

    Iterating through sheets

--- leitura_serieB.py
+++ leitura_serieB.py
@@ -3,14 +3,13 @@ import openpyxl
 
 # load excel with its path
 wrkbk = openpyxl.load_workbook("tabela_serieB.xlsx")
-
+todas_rodadas = []
 sh = wrkbk.active
-# for sheet in wb.worksheets:
-# iterate through excel and display data
-for i in range(2, sh.max_row + 1):
 
-    for j in range(1, sh.max_column + 1):
-        cell_obj = sh.cell(row=i, column=j)
+for sh in wrkbk.worksheets:
+    uma_rodada = []
+    for i in range(2, sh.max_row + 1):
+
         rodada = sh.cell(row=i, column=2).value
         mandante = sh.cell(row=i, column=3).value
         visitante = sh.cell(row=i, column=4).value
@@ -25,5 +24,9 @@ for i in range(2, sh.max_row + 1):
             total_pontos_mandante = 0
             total_pontos_visitante = 3
         classificacao_mandante = {'time': mandante, 'total_pontos': total_pontos_mandante, 'gols_pro': placar_mandante, 'gols_contra': placar_visitante, 'saldo_gols': placar_mandante-placar_visitante}
-        classificacao_visitante = {'time': visitante, 'total_pontos': total_pontos_visitante}
-        print(cell_obj.value, end=" ")
+        classificacao_visitante = {'time': visitante, 'total_pontos': total_pontos_visitante, 'gols_pro': placar_visitante, 'gols_contra': placar_mandante, 'saldo_gols': placar_visitante-placar_mandante}
+        uma_rodada.append(classificacao_visitante)
+        uma_rodada.append(classificacao_mandante)
+    todas_rodadas.append(uma_rodada)
+
+print()
